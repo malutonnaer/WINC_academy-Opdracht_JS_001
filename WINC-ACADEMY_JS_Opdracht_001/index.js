d56f9b617d24586ba77927ab74b60986b21ec29c
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-09 15:18:49 +0200

    Add files via upload

---
+++ WINC-ACADEMY_JS_Opdracht_001/index.js
@@ -0,0 +1,16 @@
+let firstName ='malu'; //string literal
+console.log(firstName);
+
+let lastName ='tonnaer'; //string literal
+console.log(lastName);
+
+let number = 16 * 2 ;
+console.log(number);
+
+let age = 35 ; //number literal
+console.log(age);
+
+let isApproved = true; // Boolean literal
+
+let selectedColor = null; //clear presence of variable
+
